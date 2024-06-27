439e3670d594e9d26365171bd5ade155efb5f822
Krishna <krishnasingh6060@gmail.com>
2024-06-27 13:16:14 +0530

    Add project files.

---
+++ BulkyFoodWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyFoodWeb.Data;
+using BulkyFoodWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyFoodWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+                _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.Category.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ BulkyFoodWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyFoodWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
