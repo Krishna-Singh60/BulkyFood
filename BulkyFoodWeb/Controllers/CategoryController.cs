e2caef51f874225d6f5fad142c5043cbe31b82b9
Krishna <krishnasingh6060@gmail.com>
2024-06-27 19:34:11 +0530

    Create Category initiaze

--- BulkyFoodWeb/Controllers/CategoryController.cs
+++ BulkyFoodWeb/Controllers/CategoryController.cs
@@ -6,17 +6,34 @@ namespace BulkyFoodWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _db;
 
-        public CategoryController(ApplicationDbContext context)
+        public CategoryController(ApplicationDbContext db)
         {
-                _context = context;
+                _db = db;
         }
         [HttpGet]
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _context.Category.ToList();
+            List<Category> objCategoryList = _db.Category.ToList();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
