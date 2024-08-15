8b5cddb19d624483e0a8ef0ce53387cf60c2017f
Krishna <krishnasingh6060@gmail.com>
2024-08-16 01:05:49 +0530

    Product Image_upload

--- BulkyFoodWeb/Areas/Admin/Controllers/ProductControlle.cs
+++ BulkyFoodWeb/Areas/Admin/Controllers/ProductControlle.cs
@@ -1,75 +1,156 @@
 ﻿using Bookie.DataAccess.Repository.IRepository;
 using Bookie.Models;
+using Bookie.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace BulkyFoodWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
-        public readonly IUnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment; //Injection webHost we can use WWWroot folder
 
-        public ProductController(IUnitOfWork unitOfWork) // Adding/Active this service to program file
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment) // Adding/Active this service to program file
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
             
         }
         public IActionResult Index()
         { 
-            List<Product> products = _unitOfWork.Product.GetAll().ToList();
-            return View(products);
+            List<Product> productsList = _unitOfWork.Product.GetAll().ToList();
+            return View(productsList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            // pass addtional funtionality
+            //Each item of Category we can be select using Projection in EF Core
+           // IEnumerable<SelectListItem> categoryList = _unitOfWork.Category.
+             //   GetAll().ToList().Select(u => new SelectListItem
+             //   {
+             //       Text = u.Name,
+               //     Value = u.Id.ToString()
+              //  });
+            //View_Bag use
+           // ViewBag.CategoryList = categoryList;
+            //View_Data Use  // Instead of that we are creating View Model and use it in Upsert View Using @Model 
+            // ViewData["CategoryList"] = categoryList;
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().
+                Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            if (id == null || id ==0)  // We are creating Product
+            {
+                //Create
+                return View(productVM);
+            }
+            else
+            {
+                //Update        // We are Updating Product
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+            
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Upsert(ProductVM productVm, IFormFile? file)
         {
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product created successfully !!";
+                string sourcePath = @"Images\product\";
+                //Uploading the the file
+                string wwwRootpath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string prodPath = Path.Combine(wwwRootpath, sourcePath);
+                    if (!string.IsNullOrEmpty(productVm.Product.ImageUrl))
+                    {
+                       //Delete Old image
+                       var oldImagePath = Path.Combine(wwwRootpath, productVm.Product.ImageUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+                    using (var fileStream = new FileStream(Path.Combine(prodPath, fileName), FileMode.Create)) // Use fileStream to read file and store
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    // Store the file in the path 
+                    productVm.Product.ImageUrl = sourcePath + fileName;
+
+                }
+                if(productVm.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVm.Product);
+                    TempData["success"] = "Product created successfully !!";
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVm.Product);
+                    TempData["success"] = "Product updated successfully !!";
+                }
+                _unitOfWork.Save();               
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                productVm.CategoryList = _unitOfWork.Category.GetAll().
+                Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(productVm);
+            }
         }
 
-        public IActionResult Edit(int? id)
-        {
+        /*  public IActionResult Edit(int? id)
+          {
 
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
+              if (id == null || id == 0)
+              {
+                  return NotFound();
+              }
 
-            // Product? catfromdb1 = _db.Product.Find(id); //only work in Promary key
-            Product? prodfromdb = _unitOfWork.Product.Get(u => u.Id == id); //Not specific to primary other tabel as well like name/display order and all 
-                                                                            // Product? catfromdb2 = _db.Product.Where(u => u.Id == id).FirstOrDefault();//find speciifc then find firstOrdefault
+              // Product? catfromdb1 = _db.Product.Find(id); //only work in Promary key
+              Product? prodfromdb = _unitOfWork.Product.Get(u => u.Id == id); //Not specific to primary other tabel as well like name/display order and all 
+                                                                              // Product? catfromdb2 = _db.Product.Where(u => u.Id == id).FirstOrDefault();//find speciifc then find firstOrdefault
 
 
-            if (prodfromdb == null)
-            {
-                return NotFound();
-            }
-            return View(prodfromdb);
-        }
+              if (prodfromdb == null)
+              {
+                  return NotFound();
+              }
+              return View(prodfromdb);
+          }
 
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                TempData["success"] = "Product updated successfully !!";
-                _unitOfWork.Save();
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
+          [HttpPost]
+          public IActionResult Edit(ProductVM obj)
+          {
+              if (ModelState.IsValid)
+              {
+                  _unitOfWork.Product.Update(obj.Product);
+                  TempData["success"] = "Product updated successfully !!";
+                  _unitOfWork.Save();
+                  return RedirectToAction("Index");
+              }
+              return View();
+          }
+        */
 
         public IActionResult Delete(int? id)
         {
--- Food.Models/Product.cs
+++ Food.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -23,16 +24,17 @@ namespace Bookie.Models
         public string Author { get; set; }
         [Required]
         [DisplayName("List Price")]
-        [Range(1,100)]
+        [Range(1,1000)]
         public double ListPrice { get; set; }
         [Required]
-        [DisplayName("List Price")]
-        [Range(1, 100)]
+        [DisplayName("Price")]
+        [Range(1, 1000)]
         public double Price { get; set; }
         [ForeignKey("CategoryId")]
-        public int CategoryId { get; set; }       
+        public int CategoryId { get; set; }
+        [ValidateNever]
         public Category Category { get; set; }
-
+        [ValidateNever]
         public string ImageUrl { get; set; }
 
     }
